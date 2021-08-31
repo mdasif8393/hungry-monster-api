928837ae39817effe5761ab19adeb3b5170c7d8e
md.asif.8393 <md.asif.8393@gmail.com>
2021-08-31 19:23:38 +0600

    complete site with responsive

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ const handleSearch = () => {
 }
 
 const searchMeals = (meal) => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`)
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`)
     .then( res => res.json())
     .then(data => showMeals(data.meals))
+    .catch(error =>handleError()) //handle api error
 }
 
 const showMeals = (meals) => {
+    document.getElementById('error-message').innerText ='';
+
     const mealsContainer = document.getElementById('meals-container');
 
-    
+    mealsContainer.innerText = ''; //empty previous search result
 
     meals.forEach((meal) => {
         
@@ -39,4 +42,37 @@ const handleSingleMeal = (mealId) => {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
     .then( res => res.json())
     .then(data => showSingleMeal(data.meals[0]))
+}
+
+const showSingleMeal = (meal) => {
+    document.getElementById('meals-container').style.display = 'none';
+    
+    const singleMeal = document.getElementById('single-meal-container');
+
+    console.log(meal);
+
+    singleMeal.innerHTML = `
+    <img class="single-image" src="${meal.strMealThumb}"> <img>
+    <h1>${meal.strMeal}</h1>
+    <h3>Ingredients</h3>
+        <p>
+        &bull; ${meal.strIngredient1} ${meal.strMeasure1}
+        </p>
+        <p>
+        &bull; ${meal.strIngredient2} ${meal.strMeasure2}
+        </p>
+        <p>
+        &bull; ${meal.strIngredient3} ${meal.strMeasure3}
+        </p>
+        <p>
+        &bull; ${meal.strIngredient4} ${meal.strMeasure4}
+        </p>
+        <p>
+        &bull; ${meal.strIngredient5} ${meal.strMeasure5}
+        </p>
+    `
+}
+
+const handleError = () => {
+    document.getElementById('error-message').innerText = "Try another meal"
 }
